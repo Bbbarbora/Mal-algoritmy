1f98dbc9b0df4365ecacc8466c9c4e8610bada2d
Barbora Lomenova <barbora.lomenova@gmail.com>
2025-04-07 08:03:56 +0200

    final

--- index.js
+++ index.js
@@ -3,83 +3,79 @@ const numbers = [
 ];
 
 // 1.Vypište do stránky všechna čísla.
-document.body.innerHTML += `Cisla pole su ${numbers} <br>`
+document.getElementById("odpoved1").innerText = `Cisla pole su ${numbers}`;
 
 // 2.Vypište do stránky druhé mocniny všech čísel.
 const druheMocniny = numbers.map((cislo) => {
     return cislo ** 2
 })
 
-document.body.innerHTML += `Druhe mocniny cisel su ${druheMocniny} <br>`
+document.getElementById("odpoved2").innerText = `Druhe mocniny cisel su ${druheMocniny}`;
 
 // 3.Vypište do stránky pouze záporná čísla.
 const zaporneCisla = numbers.map((cislo) => {
     if (cislo < 0) {
         return cislo
     }
-
 })
-document.body.innerHTML += `Zaporne cisla su ${zaporneCisla} <br>`
+document.getElementById("odpoved3").innerText = `Zaporne cisla su ${zaporneCisla}`;
 
 // 4.Vypište do stránky absolutní hodnotu všech čísel.
 const absolutnaHodnotacisla = numbers.map((cislo) => {
     return Math.abs(cislo)
-
 })
-document.body.innerHTML += `Absolutne hodnoty cisel su ${absolutnaHodnotacisla} <br>`
+document.getElementById("odpoved4").innerText = `Absolutne hodnoty cisel su ${absolutnaHodnotacisla}`;
 
 // 5.Vypište do stránky pouze sudá čísla.
 const parneCisla = numbers.map((cislo) => {
     if (cislo % 2 === 0)
         return cislo
 })
-document.body.innerHTML += `Parne cisla su ${parneCisla} <br>`
+document.getElementById("odpoved5").innerText = `Parne cisla su ${parneCisla}`;
 
 // 6.Vypište do stránky pouze ta čísla, jejíchž absolutní hodnota je dělitelná třemi.
 const cisloDelitelne3 = numbers.map((cislo) => {
     if ((Math.abs(cislo)) % 3 === 0)
         return cislo
 })
-document.body.innerHTML += `Cisla delitelne troma bezo zvysku su ${cisloDelitelne3} <br>`
+document.getElementById("odpoved6").innerText = `Cisla delitelne troma bezo zvysku su ${cisloDelitelne3}`;
 
 // 7.Vypište do stránky jak daleko je každé číslo v seznamu od čísla 5.
 const odCisla5 = numbers.map((cislo) => {
     return Math.abs(5 - cislo)
 });
 
+let vystup7 = "";
 numbers.forEach((cislo, index) => {
-    document.body.innerHTML += `Cislo ${cislo} je od cisla 5 vzdialene  ${odCisla5[index]} <br>`
+    vystup7 += `Cislo ${cislo} je od cisla 5 vzdialene  ${odCisla5[index]} \n`
 });
+document.getElementById("odpoved7").innerText = vystup7;
 
 // 8.Vypište do stránky druhé mocnicny vzdáleností všech čísel od čísla 5.
 const druheMocninyvzdialenosti = odCisla5.map((cislo) => {
     return cislo ** 2
 })
-
-document.body.innerHTML += `Druhé mocnicny vzdáleností všech čísel od čísla 5 su ${druheMocninyvzdialenosti} <br>`
+document.getElementById("odpoved8").innerText = `Druhé mocnicny vzdáleností všech čísel od čísla 5 su ${druheMocninyvzdialenosti}`;
 
 // 9.Spočítejte, kolik je v seznamu záporných čísel.
-
 let pocet = 0
 const pocetZapornychcisel = numbers.forEach((cislo) => {
     if (cislo < 0) {
         pocet++;
     }
 })
-document.body.innerHTML += `V zozname je ${pocet} záporných čísel. <br>`;
+document.getElementById("odpoved9").innerText = `V zozname je ${pocet} záporných čísel.`;
 
 // 10.Spočítejte součet všech čísel v poli.
-
 let sucet = 0
 let sucetCisel = numbers.forEach((cislo) => {
     sucet += cislo;
 })
-document.body.innerHTML += `Sucet vsetkych cisel je ${sucet}  <br>`;
+document.getElementById("odpoved10").innerText = `Sucet vsetkych cisel je ${sucet}`;
 
 // 11.Spočítejte průměr všech čísel v poli.
 const priemer = sucet / numbers.length
-
-document.body.innerHTML += `Priemer vsetkych cisel je ${priemer}  <br>`;
+document.getElementById("odpoved11").innerText = `Priemer vsetkych cisel je ${priemer}`;
 
 // 12.Spočítejte součet všech kladných čísel v poli.
 let sucetKladnychCisel = 0
@@ -88,4 +84,7 @@ const kladneCisla = numbers.forEach((cislo) => {
         return sucetKladnychCisel += cislo
     }
 })
-document.body.innerHTML += `Sucet kladnych cisel je ${sucetKladnychCisel}  <br>`;
+document.getElementById("odpoved12").innerText = `Sucet kladnych cisel je ${sucetKladnychCisel}`;
+
+
+
